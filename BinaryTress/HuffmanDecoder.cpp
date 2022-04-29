884abfeaa3257a35f15f3861db239ed8f57e6967
Onii Chan <66708802+me-oniichan@users.noreply.github.com>
2022-04-29 09:23:58 +0530

    Update HuffmanDecoder.cpp

--- BinaryTress/HuffmanDecoder.cpp
+++ BinaryTress/HuffmanDecoder.cpp
@@ -111,3 +111,23 @@ void decode_huff(node * root, string s) {
 }
 
 int main() {
+  
+    string s;
+    std::cin >> s;
+  
+    node * tree = huffman_hidden(s);
+    string code = "";
+    map<char, string>mp;
+  
+    print_codes_hidden(tree, code, mp);
+    
+    string coded;
+  
+    for( int i = 0; i < s.length(); i++ ) {
+        coded += mp[s[i]];
+    }
+    
+    decode_huff(tree,coded);
+  
+    return 0;
+}
