fa1cceb8a58d532d2bab9e34c68a788312a4514b
Codevs-py <66708802+Codevs-py@users.noreply.github.com>
2022-06-11 16:21:37 +0530

    .

--- exp.cpp
+++ exp.cpp
@@ -1,5 +1,3 @@
-<<<<<<< HEAD
-=======
 #include <iostream>
 #include <string>
 #include <algorithm>
@@ -9,4 +7,3 @@ int main() {
 	cout<<"Hello World"<<endl;
 	return 0;
 }
->>>>>>> 302dfd1e69d19fe351c26badbebf2789df8a188c
