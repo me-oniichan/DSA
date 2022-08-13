2929e0dec3c19dfb7ba94ad742332d7469cfcc7e
Codevs-py <66708802+Codevs-py@users.noreply.github.com>
2022-08-13 10:49:25 +0530

    added comments

--- Sorting/Heapsort.cpp
+++ Sorting/Heapsort.cpp
@@ -2,6 +2,7 @@
 using namespace std;
 
 void heapify(int arr[], int n, int m){
+    //to maintain heap structure
     while (m < n/2){
         int l = m;
         if (arr[l] > arr[2*l+1]) l = 2*m+1;
@@ -11,16 +12,16 @@ void heapify(int arr[], int n, int m){
         swap(arr[m], arr[l]);
         m = l;
     }
-}
+}               
 
 void minHeapify(int arr[], int n)
 {
+    //to build heap
     for (int i = n / 2 - 1; i >= 0; i--) heapify(arr, n, i);
 };
 
 void heapsort(int arr[], int n){
     minHeapify(arr, n);
-
     for(int i = n-1; i > 0; i--){
         swap(arr[0], arr[i]);
         heapify(arr, n, 0);
@@ -35,6 +36,13 @@ int main(){
     // take array input
     for (int i = 0; i < n; i++)
         cin >> arr[i];
+
+    /**
+     * @brief first build a heap, and then pick elements in sorted way while maintaining heap
+     * time complexity to build heap : O(n)
+     * time complexity to heapify : O(logn)
+     * Time complexity to sort : O(nlogn)
+     */
 
     heapsort(arr, n);
 
