52529a5a49d4c700adfd562da75feb2caa533640
Codevs-py <66708802+Codevs-py@users.noreply.github.com>
2022-08-16 08:02:55 +0530

    modified quicksort

--- Sorting/quicksort.c
+++ Sorting/quicksort.c
@@ -1,37 +1,38 @@
 #include <stdio.h>
 
-int partition(int arr[], int low, int high){
-    int pivot = arr[low];
-    int k = high;
-    for (int i = high; i > low; i--)
-    {
-        if (arr[i] > pivot){
-            arr[i] += arr[k];
-            arr[k] = arr[i] - arr[k];
-            arr[i] = arr[i] - arr[k];
-            k--;
+void swap(int *a, int *b){
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+int partition(int arr[], int start, int end){
+    int j = start-1;
+    for(int i = start; i < end; i++){
+        if (arr[i] < arr[end]){
+            j++;
+            swap(arr+j, arr+i);
         }
     }
-    arr[k] += pivot;
-    arr[low] = arr[k] - pivot;
-    arr[k] = arr[k] - arr[low];
-    return k;
-}
+    swap(arr+j+1, arr+end);
+    return j+1;
+};
 
-void quickSort(int arr[], int high, int low){
-    if (low < high){
-        int index = partition(arr, low, high);
-        quickSort(arr, index-1, low);
-        quickSort(arr, high, index+1);
-    }
-}
+void quicksort(int arr[], int start, int end){
+    if (end <= start) return;
+    int q = partition(arr, start, end);
+    quicksort(arr, start, q-1);
+    quicksort(arr, q+1, end);
+};
 
 int main(){
-    int arr[] = {43,23,4,5,3,4,65,4}, size = 8;
-    quickSort(&arr[0], size-1, 0);
-    for (int i = 0; i < size; i++)
-    {
-        printf("%d ", arr[i]);
-    }
+    int n;
+    scanf("%d", &n);
+    int arr[n];
+    for(int i =0; i < n; i++) scanf("%d", arr+i);
+
+    quicksort(arr, 0, n-1);
+
+    for(int i=0; i < n; i++) printf("%d ", arr[i]);
     return 0;
 }
