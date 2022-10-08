b478c70f0f6a017667cc9882f6cb824040c1e906
Codevs-py <66708802+Codevs-py@users.noreply.github.com>
2022-10-08 16:29:24 +0530

    added left right rotation to avl tree

--- Trees/AVLTree.cpp
+++ Trees/AVLTree.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 class Node{
     public:
-    int data;
+    int data, height = 1;
     Node* left = NULL;
     Node* right = NULL;
     Node* parent = NULL;
@@ -14,6 +14,12 @@ class Node{
     }
 
     Node(){};
+
+    int heightbalance(){
+        int left_height = (left != nullptr)? left->height : 0;
+        int right_height = (right != nullptr)? right->height : 0;
+        return left_height - right_height;
+    }
 };
 
 class AVL{
@@ -22,9 +28,70 @@ class AVL{
 
     void insert(int, Node*, Node*);
     void inorder(Node*);
-    void right_rotate(Node *);
     void level_order();
+    void right_rotate(Node*);
+    void left_rotate(Node*);
 };
+
+
+void AVL:: inorder(Node* node){
+    if(node==nullptr) return;
+    inorder(node->left);
+    cout<<node->data<<' ';
+    inorder(node->right);
+}
+
+void AVL::level_order(){
+    if(root == nullptr) return;
+    queue<Node*> q;
+    q.push(root);
+    while(!q.empty()){
+        Node* node = q.front();
+        if(node->left != nullptr) q.push(node->left);
+        if(node->right != nullptr) q.push(node->right);
+        cout<<node->data<<' ';
+        q.pop();
+    }
+    cout<<endl;
+}
+
+int height(Node* node){
+    if(node == nullptr) return 0;
+    return node->height;
+}
+
+void AVL:: right_rotate(Node * node){
+    Node *left = node->left;
+    
+    node->left = left->right;
+    if(node->left != nullptr) node->left->parent = node;
+    
+    left->right = node;
+    left->parent = node->parent;
+    node->parent = left;
+    if(left->parent != nullptr) left->parent->left = left;
+    else {
+        root = left;
+    }
+    node->height = 1+max(height(node->left), height(node->right));
+    left->height = 1+max(height(left->left), height(left->right));
+}
+
+void AVL:: left_rotate(Node* node){
+    Node *right = node->right;
+    
+    node->right = right->left;
+    right->left = node;
+    right->parent = node->parent;
+    node->parent = right;
+    if(right->parent == nullptr) root = right;
+    else {
+         right->parent->right = right;
+    }
+
+    node->height = 1+max(height(node->left), height(node->right));
+    right->height = 1+max(height(right->left), height(right->right));
+}
 
 void AVL:: insert(int data, Node* node, Node *parent = NULL){
     if (root == nullptr){
@@ -39,40 +106,22 @@ void AVL:: insert(int data, Node* node, Node *parent = NULL){
 
     if(node->data < data) insert(data, node->right, node);
     else insert(data, node->left, node);
-}
 
-void AVL:: inorder(Node* node){
-    if(node==nullptr) return;
-    inorder(node->left);
-    cout<<node->data<<' ';
-    inorder(node->right);
-}
+    node->height = 1+ max(height(node->left), height(node->right));
 
-void AVL:: right_rotate(Node * node){
-    Node *left = node->left;
-    
-    node->left = left->right;
-    left->right = node;
-    left->parent = node->parent;
-    node->parent = left;
-    if(left->parent != nullptr) left->parent->left = left;
-    else {
-        root = left;
+    int balance = node->heightbalance();
+
+    if (balance > 1 && data < node->left->data) right_rotate(node);
+    else if (balance < -1 && data > node->right->data) left_rotate(node);
+    if (balance > 1 && data > node->left->data){
+        left_rotate(node->left);
+        right_rotate(node);
     }
+    else if (balance <-1 && data < node->right->data){
+        right_rotate(node->right);
+        left_rotate(node);
+    };
 }
-
-void AVL::level_order(){
-    queue<Node*> q;
-    q.push(root);
-    while(!q.empty()){
-        if(root = nullptr) return;
-        Node* node = q.front();
-        if(node->left != nullptr) q.push(node->left);
-        if(node->right != nullptr) q.push(node->right);
-        cout<<node->data<<' ';
-        q.pop();
-    }
-} 
 
 int main(){
     AVL tree;
@@ -81,8 +130,7 @@ int main(){
     tree.insert(7, tree.root);
     tree.insert(4, tree.root);
     tree.insert(2, tree.root);
-    tree.right_rotate(tree.root);
-    tree.level_order();                                         
+    tree.level_order();                                
 
 return 0;
 }
