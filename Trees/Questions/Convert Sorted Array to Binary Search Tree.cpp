fa818596da03e7b6c20aa46b0d93ad3c3d988ee8
Codevs-py <66708802+Codevs-py@users.noreply.github.com>
2022-12-10 12:39:37 +0530

    added new question

--- Trees/Questions/Convert Sorted Array to Binary Search Tree.cpp
+++ Trees/Questions/Convert Sorted Array to Binary Search Tree.cpp
@@ -1,8 +1,29 @@
 //Problem Statement : https://leetcode.com/explore/interview/card/top-interview-questions-easy/94/trees/631/
 
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
 
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+public:
+    TreeNode* reqNode(vector<int>& nums, int low, int high){
+        if (low> high) return NULL;
+        int mid = (low+high)/2;
+        TreeNode* node = new TreeNode(nums[mid], reqNode(nums, low, mid-1), reqNode(nums, mid+1, high));
+        return node;
+    };
+    TreeNode* sortedArrayToBST(vector<int>& nums) {
+        return reqNode(nums, 0, nums.size()-1);
+    }
+};
  
  
 int main(){
