8cf2895dc98745a038e64400ba1e9cf6fa98972b
Codevs-py <66708802+Codevs-py@users.noreply.github.com>
2023-02-20 12:29:44 +0530

    day 51

---
+++ Leetcode Daily/20-02-2023.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define fii for(int i = 0; i < n; i++)
+
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        return lower_bound(nums.begin(), nums.end(), target) - nums.begin();
+    }
+};
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    
+return 0;
+}
