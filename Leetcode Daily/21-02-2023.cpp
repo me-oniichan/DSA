51d32d4cd9171a817dc231ba934e0a4df6a61556
Codevs-py <66708802+Codevs-py@users.noreply.github.com>
2023-02-21 11:30:47 +0530

    day 52

---
+++ Leetcode Daily/21-02-2023.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define fii for(int i = 0; i < n; i++)
+
+class Solution {
+public:
+    int singleNonDuplicate(vector<int>& nums) {
+        int left = 0, right = nums.size() - 1;
+        while(left < right){
+            int mid = (left + right)/2;
+            if((mid % 2 == 0 && nums[mid] == nums[mid + 1]) || (mid % 2 == 1 && nums[mid] == nums[mid - 1]))
+                left = mid + 1;
+            else
+                right = mid;
+        }
+        
+        return nums[left];
+    }
+};
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    
+return 0;
+}
