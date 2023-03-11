f873432c53a015755504278c21f16b0a1b7191be
Codevs-py <66708802+Codevs-py@users.noreply.github.com>
2023-03-11 19:42:31 +0530

    day 70

---
+++ Leetcode Daily/11-03-2023.java
@@ -0,0 +1,40 @@
+import java.util.*;;
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode() {}
+    ListNode(int val) { this.val = val; }
+    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+}
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode() {}
+    TreeNode(int val) { this.val = val; }
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+
+class Solution {
+    public TreeNode sortedListToBST(ListNode head) {
+        List<Integer> list = new ArrayList<>();
+        while (head != null) {
+            list.add(head.val);
+            head = head.next;
+        }
+        return buildBST(list, 0, list.size() - 1);
+    }
+    TreeNode buildBST(List<Integer> list, int left, int right) {
+        if (left > right) return null;
+        int mid = left + (right - left) / 2;
+        TreeNode root = new TreeNode(list.get(mid));
+        root.left = buildBST(list, left, mid - 1);
+        root.right = buildBST(list, mid + 1, right);
+        return root;
+    }
+}
