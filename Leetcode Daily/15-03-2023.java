85d4edd8f5d27096ca42851b1de25cf7953c3e69
Codevs-py <66708802+Codevs-py@users.noreply.github.com>
2023-03-15 17:37:58 +0530

    day 74

---
+++ Leetcode Daily/15-03-2023.java
@@ -0,0 +1,49 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+import java.util.TreeSet;
+
+import javax.swing.text.html.HTMLDocument.RunElement;
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode() {}
+    TreeNode(int val) { this.val = val; }
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+class Solution {
+    int prev = -1; boolean ans=true;
+    Set<Integer> s = new TreeSet<>();
+    void dfs(TreeNode node, int h){
+        if(node==null){
+            s.add(h);
+            if(s.size()>2){
+                ans=false;
+                return;
+            }
+            else if(prev==-1){
+                prev=h; return;
+            }
+            int diff = prev-h;
+            prev=h;
+            if(diff==0 || diff==1) return;
+            ans=false;
+            return;
+        }
+        if(ans){
+            dfs(node.left, h+1);
+            dfs(node.right, h+1);
+        }
+    }
+    public boolean isCompleteTree(TreeNode root) {
+        dfs(root, 0);
+        return ans;
+    }
+}
