9dab66c2daaad082328816d6410d6f9a66b07d90
Nii~ <allena15243@gmail.com>
2024-06-17 09:36:09 +0530

    day 533

---
+++ Leetcode Daily/Sum of Square Numbers.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    bool judgeSquareSum(int c) {
+        for (long a = 0; a * a <= c; a++) {
+            double b = sqrt(c - a * a);
+            if (b == (int)b) {
+                return true;
+            }
+        }
+        return false;
+    }
+};
